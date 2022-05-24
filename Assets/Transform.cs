9e2e62d359e7a7da4d4c7d5a90a58d59d311b6f2
BrandonStock1 <46919113@est.ort.edu.ar>
2022-05-24 10:02:15 -0300

    finish ej 1 and 2 colission

---
+++ Assets/Transform.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transform : MonoBehaviour
+{
+    public float rotationspeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.eulerAngles += new Vector3(0, rotationspeed, 0);
+    }
+}
